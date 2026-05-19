5286f7f91c54fb422223f8261499ff1a1d263eef
Leo Omi <leoyuuki@gmail.com>
2026-05-19 20:18:11 -0300

    cherry pick changes from desktop

---
+++ hyprland/frappe.lua
@@ -0,0 +1,81 @@
+local colors = {
+  rosewater = "rgb(f2d5cf)",
+  rosewaterAlpha = "rgba(f2d5cfee)",
+
+  flamingo = "rgb(eebebe)",
+  flamingoAlpha = "rgba(eebebeee)",
+
+  pink = "rgb(f4b8e4)",
+  pinkAlpha = "rgba(f4b8e4ee)",
+
+  mauve = "rgb(ca9ee6)",
+  mauveAlpha = "rgba(ca9ee6ee)",
+
+  red = "rgb(e78284)",
+  redAlpha = "rgba(e78284ee)",
+
+  maroon = "rgb(ea999c)",
+  maroonAlpha = "rgba(ea999cee)",
+
+  peach = "rgb(ef9f76)",
+  peachAlpha = "rgba(ef9f76ee)",
+
+  yellow = "rgb(e5c890)",
+  yellowAlpha = "rgba(e5c890ee)",
+
+  green = "rgb(a6d189)",
+  greenAlpha = "rgba(a6d189ee)",
+
+  teal = "rgb(81c8be)",
+  tealAlpha = "rgba(81c8beee)",
+
+  sky = "rgb(99d1db)",
+  skyAlpha = "rgba(99d1dbee)",
+
+  sapphire = "rgb(85c1dc)",
+  sapphireAlpha = "rgba(85c1dcee)",
+
+  blue = "rgb(8caaee)",
+  blueAlpha = "rgba(8caaeeee)",
+
+  lavender = "rgb(babbf1)",
+  lavenderAlpha = "rgba(babbf1ee)",
+
+  text = "rgb(c6d0f5)",
+  textAlpha = "rgba(c6d0f5ee)",
+
+  subtext1 = "rgb(b5bfe2)",
+  subtext1Alpha = "rgba(b5bfe2ee)",
+
+  subtext0 = "rgb(a5adce)",
+  subtext0Alpha = "rgba(a5adceee)",
+
+  overlay2 = "rgb(949cbb)",
+  overlay2Alpha = "rgba(949cbbee)",
+
+  overlay1 = "rgb(838ba7)",
+  overlay1Alpha = "rgba(838ba7ee)",
+
+  overlay0 = "rgb(737994)",
+  overlay0Alpha = "rgba(737994ee)",
+
+  surface2 = "rgb(626880)",
+  surface2Alpha = "rgba(626880ee)",
+
+  surface1 = "rgb(51576d)",
+  surface1Alpha = "rgba(51576dee)",
+
+  surface0 = "rgb(414559)",
+  surface0Alpha = "rgba(414559ee)",
+
+  base = "rgb(303446)",
+  baseAlpha = "rgba(303446ee)",
+
+  mantle = "rgb(292c3c)",
+  mantleAlpha = "rgba(292c3cee)",
+
+  crust = "rgb(232634)",
+  crustAlpha = "rgba(232634ee)",
+}
+
+return colors
